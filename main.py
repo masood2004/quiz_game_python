f7322ec9aec7ec5f27223d8ce7472b67c5dd2cc2
Syed Masood Hussain <hmasood3288@gmail.com>
2024-12-28 19:18:36 +0500

    made questions_bank using for loop

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from question_model import Question
 from data import question_data
+
+questions_bank = []  # Initialize the list to store the questions
+
+questions_len = len(question_data)
+
+for x in range(questions_len):
+    questions_bank.append(
+        Question(question_data[x]['text'], question_data[x]['answer']))
+
+print(questions_bank)
