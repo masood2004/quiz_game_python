48a97b44a7f2d9ee4a28f6aa0a3772311a7f7987
Syed Masood Hussain <hmasood3288@gmail.com>
2024-12-29 15:22:58 +0500

    more implementions in QuizBrain Class

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 questions_bank = []  # Initialize the list to store the questions
 
 questions_len = len(question_data)
 
 for x in range(questions_len):
+    # For Loop to retrieve the questions from data and append them into the questions_bank list.
     questions_bank.append(
         Question(question_data[x]['text'], question_data[x]['answer']))
 
-print(questions_bank)
+
+quiz = QuizBrain(questions_bank)
+quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,15 @@
 class QuizBrain:
-    def __init__(self, question_number, question_list):
+    def __init__(self, question_list):
+        """Initializing the QuizBrain Class"""
         self.question_number = 0
         self.question_list = question_list
+
+    def next_question(self):
+        """Function to ask the questions"""
+        user_choice = input(f"Q.{self.question_number +
+                                 1}: {self.question_list[0].text} (True/False)?: ")
+        self.question_number += 1
+
+    def still_has_questions(self):
+        """Function to check if the user gave the correct answer or not?"""
+        if user_choice
